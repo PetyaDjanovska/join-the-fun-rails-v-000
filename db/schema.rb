88eb8bfae5bd2966817d277722dd1809cbe8cf11
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 12:56:46 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20140212205318) do
   end
 
   create_table "rides", force: :cascade do |t|
+    t.integer :passanger_id
+    t.integer :taxi_id
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
